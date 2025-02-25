4ce1e594e1bd68514a57bfb0949097e122d64685
BRIAN AKASHABA <akashaba@gmail.com>
2025-02-25 18:27:09 +0000

    updated stock price checker

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,135 @@
 'use strict';
+require('dotenv').config();
+const axios = require('axios');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+mongoose.connect(process.env.DB)
+.then(() => {
+  console.log("Connected to database!");
+})
+.catch((err) => {
+  console.error(err);
+})
+
+const stockSchema = new mongoose.Schema({
+  stock: String,
+  likes: Number,
+  ips: [String]
+});
+
+const Stock = mongoose.model('Stock', stockSchema);
 
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
-    .get(function (req, res){
-      
+    .get(async function (req, res){
+      const hash = bcrypt.hashSync(req.ip, 10);
+      if (Array.isArray(req.query.stock)) {
+        const response1 = await axios.get(`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${req.query.stock[0]}/quote`);
+        const response2 = await axios.get(`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${req.query.stock[1]}/quote`);
+        let data1 = await Stock.findOne({stock: response1.data.symbol}).exec();
+        let data2 = await Stock.findOne({stock: response2.data.symbol}).exec();
+        if (data1 == null) {
+          const newStock1 = new Stock({
+            stock: response1.data.symbol,
+            likes: 0,
+            ips: []
+          });
+          newStock1.save();
+          data1 = newStock1;
+        }
+
+        if (data2 == null) {
+          const newStock2 = new Stock({
+            stock: response2.data.symbol,
+            likes: 0,
+            ips: []
+          });
+          newStock2.save();
+          data2 = newStock2;
+        }
+        res.json({
+          stockData: [{
+            stock: response1.data.symbol,
+            price: response1.data.close,
+            rel_likes: data1.likes - data2.likes
+          }, {
+            stock: response2.data.symbol,
+            price: response2.data.close,
+            rel_likes: data2.likes - data1.likes
+          }]
+        });
+        let isNew = !data1.ips.some(hsh => bcrypt.compareSync(req.ip, hsh));
+        if (req.query.like && isNew) {
+          const updateStock1 = await Stock.findOneAndUpdate({stock: response1.data.symbol}, 
+            {
+              $inc: {
+                likes: 1
+              },
+              $push: {
+                ips: hash
+              }
+            }, {new: true});
+          const updateStock2 = await Stock.findOneAndUpdate({stock: response2.data.symbol}, 
+            {
+              $inc: {
+                likes: 1
+              },
+              $push: {
+                ips: hash
+              }
+            }, {new: true});
+        }
+      } else {
+        const response = await axios.get(`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${req.query.stock}/quote`);
+        const data = await Stock.findOne({stock: response.data.symbol}).exec();
+        if (data == null) {
+          const newStock = new Stock({
+            stock: response.data.symbol,
+            likes: 0,
+            ips: []
+          });
+          newStock.save()
+          .then(savedStock => {
+            res.json({
+              stockData: {
+                stock: response.data.symbol,
+                price: response.data.close,
+                likes: savedStock.likes
+              }
+            });
+          })
+        } else {
+          let isNew = !data.ips.some(hsh => bcrypt.compareSync(req.ip, hsh));
+          if (req.query.like && isNew) {
+            const updateStock = await Stock.findOneAndUpdate({stock: response.data.symbol}, 
+              {
+                $inc: {
+                  likes: 1
+                },
+                $push: {
+                  ips: hash
+                }
+              }, {new: true});
+            res.json({
+              stockData: {
+                stock: response.data.symbol,
+                price: response.data.close,
+                likes: updateStock.likes
+              }
+            });
+          } else {
+            res.json({
+              stockData: {
+                stock: response.data.symbol,
+                price: response.data.close,
+                likes: data.likes
+              }
+            });
+          }
+        }
+      }
     });
     
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,81 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-
+    this.timeout(5000);
+    test('GET /api/stock-prices?stock=[stock]', function (done) {
+        chai
+            .request(server)
+            .keepOpen()
+            .get('/api/stock-prices')
+            .set('content-type', 'application/json')
+            .query({stock: 'goog'})
+            .end(function (err, res) {
+                if (err) return console.error(err);
+                assert.equal(res.status, 200);
+                assert.isString(res.body.stockData.stock);
+                assert.isNumber(res.body.stockData.price);
+                assert.isNumber(res.body.stockData.likes);
+                done();
+            });
+    });
+    test('GET /api/stock-prices?stock=[stock]&like=true', function (done) {
+        chai
+            .request(server)
+            .get('/api/stock-prices')
+            .set('content-type', 'application/json')
+            .query({stock: 'goog', like: true})
+            .end(function (err, res) {
+                if (err) return console.error(err);
+                assert.equal(res.status, 200);
+                assert.isString(res.body.stockData.stock);
+                assert.isNumber(res.body.stockData.price);
+                assert.isNumber(res.body.stockData.likes);
+                done();
+            });
+    });
+    test('GET /api/stock-prices?stock=[stock]&like=true', function (done) {
+        chai
+            .request(server)
+            .get('/api/stock-prices')
+            .set('content-type', 'application/json')
+            .query({stock: 'goog', like: true})
+            .end(function (err, res) {
+                if (err) return console.error(err);
+                assert.equal(res.status, 200);
+                assert.isString(res.body.stockData.stock);
+                assert.isNumber(res.body.stockData.price);
+                assert.isNumber(res.body.stockData.likes);
+                done();
+            });
+    });
+    test('GET /api/stock-prices?stock=[stock]&stock=[stock]', function (done) {
+        chai
+            .request(server)
+            .get('/api/stock-prices')
+            .set('content-type', 'application/json')
+            .query({stock: ['goog','tsla']})
+            .end(function (err, res) {
+                if (err) return console.error(err);
+                assert.equal(res.status, 200);
+                assert.isString(res.body.stockData[0].stock);
+                assert.isNumber(res.body.stockData[0].price);
+                assert.isNumber(res.body.stockData[0].rel_likes);
+                done();
+            });
+    });
+    test('GET /api/stock-prices?stock=[stock]&stock=[stock]&like=true', function (done) {
+        chai
+            .request(server)
+            .get('/api/stock-prices')
+            .set('content-type', 'application/json')
+            .query({stock: ['goog', 'tsla'], like: true})
+            .end(function (err, res) {
+                if (err) return console.error(err);
+                assert.equal(res.status, 200);
+                assert.isString(res.body.stockData[0].stock);
+                assert.isNumber(res.body.stockData[0].price);
+                assert.isNumber(res.body.stockData[0].rel_likes);
+                done();
+            });
+    });
 });
